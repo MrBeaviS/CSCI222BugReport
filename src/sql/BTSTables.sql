50b8f989f8c2f820314d6278080e0b1a157a9307
MrBeaviS <mdbeavis@gmail.com>
2016-10-12 20:03:48 +1100

    Add files via upload
    Altered BTSTables.sql so that tables are created normally now, also pushed a drop tables script.

--- src/sql/BTSTables.sql
+++ src/sql/BTSTables.sql
@@ -7,17 +7,17 @@ CREATE TABLE SuperUser (
 	Password varchar(16),
 	JoinedDate date,
 	PRIMARY KEY(UserID)
-)
+);
 
 CREATE TABLE RegisteredUser (
 	UserID int NOT NULL,
 	UserReputation ENUM('New', 'OK', 'Good', 'Great', 'AWESOME!', 'Bad'),
 	BugReport int,
-	AccountStatus ENUM('Active', 'Suspended', 'Deleted')
+	AccountStatus ENUM('Active', 'Suspended', 'Deleted'),
 	FOREIGN KEY (UserID)
 		REFERENCES SuperUser(UserID)
 		ON DELETE CASCADE
-)
+);
 
 CREATE TABLE SysAdminUser (
 	UserID int NOT NULL,
@@ -27,14 +27,46 @@ CREATE TABLE SysAdminUser (
 	AttachmentID int,
 	FOREIGN KEY (UserID)
 		REFERENCES SuperUser(UserID)
-		ON DELETE CASCADE,
-	FOREIGN KEY (AssignedBug)
-		REFERENCES BugReports(BugReportID),
-	FOREIGN KEY (CommentID)
-		REFERENCES Comments(CommentID),
+		ON DELETE CASCADE
+);
+
+CREATE TABLE Attachments (
+	AttachmentID int NOT NULL,
+	CreationTimestamp date,
+	ShortDescription varchar(200),
+	FileName varchar(50),
+	FileType varchar(50),
+	FileSize int,
+	AttacherID int,
+	PRIMARY KEY (AttachmentID),
+	FOREIGN KEY (AttacherID)
+		REFERENCES SysAdminUser(UserID)
+);
+
+CREATE TABLE Comments (
+	CommentID int,
+	UserID int,
+	CreationTimestamp date,
+	CommentText varchar(500),
+	AttachmentID int,
+	PRIMARY KEY (CommentID),
+	FOREIGN KEY (UserID)
+		REFERENCES SuperUser(UserID),
 	FOREIGN KEY (AttachmentID)
 		REFERENCES Attachments(AttachmentID)
-)
+);
+
+CREATE TABLE Bug (
+	BugID int NOT NULL AUTO_INCREMENT,
+	BugName varchar(50),
+	BugReportID int,
+	Product varchar(50),
+	Component varchar(50),
+	Version varchar(10),
+	OperatingSystem varchar(50),
+	PRIMARY KEY (BugID)
+);
+
 
 CREATE TABLE BugReports (
 	BugReportID int NOT NULL AUTO_INCREMENT,
@@ -59,43 +91,4 @@ CREATE TABLE BugReports (
 		REFERENCES SysAdminUser(UserID),
 	FOREIGN KEY (CommentID)
 		REFERENCES Comments(CommentID)
-)
-
-CREATE TABLE Comments (
-	CommentID int,
-	UserID int,
-	CreationTimestamp date,
-	CommentText varchar(500),
-	AttachmentID int,
-	PRIMARY KEY (CommentID),
-	FOREIGN KEY (UserID)
-		REFERENCES SuperUser(UserID),
-	FOREIGN KEY (AttachmentID)
-		REFERENCES Attachments(AttachmentID)
-)
-
-CREATE TABLE Bug (
-	BugID int NOT NULL AUTO_INCREMENT,
-	BugName varchar(50),
-	BugReportID int,
-	Product varchar(50),
-	Component varchar(50),
-	Version varchar(10),
-	OperatingSystem varchar(50),
-	PRIMARY KEY (BugID),
-	FOREIGN KEY (BugReportID)
-		REFERENCES BugReports(BugReportID)
-)
-
-CREATE TABLE Attachments (
-	AttachmentID int NOT NULL,
-	CreationTimestamp date,
-	ShortDescription varchar(200),
-	FileName varchar(50),
-	FileType varchar(50),
-	FileSize int,
-	AttacherID int,
-	PRIMARY KEY (AttachmentID),
-	FOREIGN KEY (AttacherID)
-		REFERENCES SysAdminUser(UserID)
-)
+);
